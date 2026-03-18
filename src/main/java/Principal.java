d34c19a06dfbfdce6fe18a3199569d2264f9a71d
10726112930 <10726112930@UNS07DT99015.animaedu.intranet>
2026-03-17 21:14:24 -0300

    init: comentários

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -3,10 +3,13 @@ import javax.swing.JOptionPane;
 public class Principal {
 
     public static void main(String[] args) {
+        //Variáveis
         double base, altura, area;
+        //Entradas
         base = Double.parseDouble(JOptionPane.showInputDialog("Digite a base: "));
         altura = Double.parseDouble(JOptionPane.showInputDialog("Digite a altura: "));
         area = (base * altura)/2.0;
+        //Saída
         JOptionPane.showMessageDialog (null, "A area é" + area);
     }
     
